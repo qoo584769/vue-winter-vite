395f92da785889c46be2f50cc47c410d329fc0d0
ming wu <a85062@pchome.com.tw>
2023-02-12 01:11:28 +0800

    deploy

--- assets/AboutView-7b1caeb8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as t,d as s,f as o}from"./index-8de78fd8.js";const _={},c={class:"about"},n=o("h1",null,"This is an about page",-1),a=[n];function i(r,d){return t(),s("div",c,a)}const l=e(_,[["render",i]]);export{l as default};
