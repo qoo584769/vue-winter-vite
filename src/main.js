37044c75474f18b4cd7fd8ad2ba18b02fbefffe4
ming wu <a85062@pchome.com.tw>
2023-02-12 00:12:53 +0800

    修正語系問題

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import "vue-loading-overlay/dist/css/index.css";
 import { defineRule, configure } from "vee-validate";
 import AllRules from "@vee-validate/rules";
 import { localize, setLocale } from "@vee-validate/i18n";
-import zhTW from "@vee-validate/i18n/dist/locale/zh_TW.json";
+import zh_TW from "@vee-validate/i18n/dist/locale/zh_TW.json";
 
 // 載入全部驗證
 Object.keys(AllRules).forEach((rule) => {
@@ -29,7 +29,7 @@ Object.keys(AllRules).forEach((rule) => {
 // loadLocaleFromURL("../zh_TW.json");
 // 把本地語系加入主套件
 configure({
-  generateMessage: localize({ zh_TW: zhTW }),
+  generateMessage: localize({ zh_TW }),
   validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
 });
 setLocale("zh_TW");
